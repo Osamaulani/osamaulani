288cd970f1e8b7cc668dc72f757b56a648d8f72d
Osamaulani <135732887+Osamaulani@users.noreply.github.com>
2025-01-30 13:38:33 +0700

    Add files via upload

---
+++ tubesosa.py
@@ -0,0 +1,21 @@
+Toko1 = {'Beras1' : 15000, 'Minyak1' : 32500, 'Gula1' : 12000, 'Terigu1' : 7000, 'Telur1' : 24000}
+Toko2 = {'Beras2' : 14500, 'Minyak2' : 32500, 'Gula2' : 12000, 'Terigu2' : 7500, 'Telur2' : 24000}
+Toko3 = {'Beras3' : 15000, 'Minyak3' : 33000, 'Gula3' : 12000, 'Terigu3' : 7000, 'Telur3' : 24500}
+Toko4 = {'Beras4' : 16000, 'Minyak4' : 34000, 'Gula4' : 14000, 'Terigu4' : 8000, 'Telur4' : 26000}
+Toko5 = {'Beras5' : 12500, 'Minyak5' : 32500, 'Gula5' : 12000, 'Terigu5' : 6000, 'Telur5' : 22000}
+Toko6 = {'Beras6' : 15000, 'Minyak6' : 33000, 'Gula6' : 12000, 'Terigu6' : 8000, 'Telur6' : 23000}
+
+toko = {**Toko1, **Toko2, **Toko3, **Toko4, **Toko5, **Toko6}
+print(toko)
+
+
+
+
+
+
+
+
+
+
+
+
